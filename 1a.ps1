fbf143274bd09bd342d57ea513fd37216d30736e
Peter Bajurny <bajurny@umn.edu>
2020-12-01 10:58:25 -0600

    day 1

---
+++ 1a.ps1
@@ -0,0 +1,10 @@
+﻿[int[]]$expenses = get-content 1.txt
+
+foreach ($inner in $expenses) {
+    foreach ($outer in $expenses) {
+        if (($inner + $outer) -eq 2020) {
+            write-host "$inner $outer $($inner*$outer)"
+            return
+        }
+    }
+}
---
+++ 1b.ps1
@@ -0,0 +1,12 @@
+﻿[int[]]$expenses = get-content 1.txt
+
+foreach ($inner in $expenses) {
+    foreach ($middle in $expenses) {
+        foreach ($outer in $expenses) {
+            if (($inner + $middle + $outer) -eq 2020) {
+                write-host "$inner $middle $outer $($inner*$middle*$outer)"
+                return
+            }
+        }
+    }
+}
