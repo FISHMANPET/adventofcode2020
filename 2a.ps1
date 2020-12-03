f20082135515e86f832ecc48816cc960b2b9548d
Peter Bajurny <bajurny@umn.edu>
2020-12-02 20:05:27 -0600

    day 2

---
+++ 2a.ps1
@@ -0,0 +1,15 @@
+﻿$passwords = get-content '2.txt'
+
+$correct = 0
+
+foreach ($entry in $passwords) {
+    $line = $entry.split()
+    $min, $max = $line[0].split('-')
+    [char]$char = $line[1][0]
+    $pw = $line[2]
+    $count = ($pw.ToCharArray() | Where-Object {$_ -eq $char} | Measure-Object -Character).Characters
+    if ($count -ge $min -and $count -le $max) {
+        $correct++
+    }
+}
+Write-Host "$correct correct passwords"
