73500c8e9889a7f44145b480f2435dd2f3d88b49
Peter Bajurny <bajurny@umn.edu>
2020-12-04 12:50:36 -0600

    day 3

---
+++ 3a.ps1
@@ -0,0 +1,13 @@
+﻿$map = get-content '3.txt'
+
+$trees = 0
+$position = 0
+$mapwidth = $map[0].Length
+foreach ($row in $map) {
+    if ($row[$position%$mapwidth] -eq '#') {
+        $trees++
+    }
+    $position += 3
+}
+
+Write-Host "$trees trees hit"
---
+++ 3b.ps1
@@ -0,0 +1,30 @@
+﻿$map = get-content '3.txt'
+
+$trees = @()
+$mapwidth = $map[0].Length
+$maplength = $map.Length
+$scenarios = @(
+    @{r = 1; d = 1}
+    @{r = 3; d = 1}
+    @{r = 5; d = 1}
+    @{r = 7; d = 1}
+    @{r = 1; d = 2}
+)
+foreach ($s in $scenarios) {
+    $position = 0
+    $localtrees = 0
+    for ($i = 0; $i -lt $maplength; $i += $s.d) {
+        if ($map[$i][$position % $mapwidth] -eq '#') {
+            $localtrees++
+        }
+        $position += $s.r
+    }
+    $trees += $localtrees
+}
+
+$multiply = 1
+foreach ($count in $trees) {
+    $multiply = $multiply * $count
+}
+
+Write-Host "$multiply trees hit"
